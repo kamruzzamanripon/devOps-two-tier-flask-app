cf46bc0bb916b3d78f2d58c5c83768c5150cbd4a
rkamruzzamanripon <rkamruzzaman@gmail.com>
2024-12-03 00:41:45 +0600

    update k8 folder

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 # Configure MySQL from environment variables
 app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
+#app.config['MYSQL_HOST'] = "mysql"
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'default_user')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'default_password')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'default_db')
